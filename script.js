83759ee6ffe480c6a5a993305c032e5c421b4d37
Germeson Martins <65938576+germeson-martins@users.noreply.github.com>
2025-05-10 12:43:34 -0300

    Update Modal Card

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const cartCounter = document.getElementById("cart-count")
 const addressinput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = [];
+
 
 //Abrir Modal do Carrinho
 cartBtn.addEventListener("click", function() {
+    updateCartModal()
     cartModal.style.display = "flex"
 })
 
@@ -31,12 +34,77 @@ closeModalBtn.addEventListener("click", function() {
 menu.addEventListener("click", function(event) {
     // console.log(event.target)
     let parentButton = event.target.closest(".add-to-cart-btn")
+    // console.log(parentButton)
     if(parentButton){
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
 
-   
-        
+        // console.log(name)
+        // console.log(price)
+
+        addToCart(name, price)
+      
     }
 
 })
+
+// Função para Adicionar ao Carrinho
+function addToCart(name,price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        //Se o item já existe, aumenta apenas a quantidade + 1
+        // console.log(existingItem);
+        existingItem.quantity += 1;
+
+    }else{
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+
+    }
+
+    updateCartModal()
+
+}
+
+//Atualiza Carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        // console.log(item);
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class="font-medium mt-3">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+            </div>
+
+                <button>
+                    Remover
+                </button>
+
+        </div>
+        `
+        total += item.price * item.quantity;
+        
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+
+}
